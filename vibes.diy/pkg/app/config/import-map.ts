578c5af76deaad42695b6e9fc5723dc527ff7e93
J Chris Anderson <jchris@fireproof.storage>
2025-11-23 11:26:24 +0100

    chore: update VIBES_VERSION to 0.18.3-dev-debug-log in import map

--- vibes.diy/pkg/app/config/import-map.ts
+++ vibes.diy/pkg/app/config/import-map.ts
@@ -3,7 +3,7 @@
  * Used by: root.tsx, eject-template.ts, hosting packages
  */
 
-const VIBES_VERSION = "0.18.1";
+const VIBES_VERSION = "0.18.3-dev-debug-log";
 
 export function getLibraryImportMap() {
   return {
