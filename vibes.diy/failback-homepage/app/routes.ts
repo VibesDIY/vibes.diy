f25f9246a7239349363939c811ed2e8e8e7ec651
Amber Macias <arussek05@gmail.com>
2026-01-09 14:34:53 -0600

    always redirect to homescreen

--- vibes.diy/failback-homepage/app/routes.ts
+++ vibes.diy/failback-homepage/app/routes.ts
@@ -1,11 +1,5 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
-// import { flatRoutes } from "@react-router/fs-routes";
 
-// Enrutamiento original comentado temporalmente
-// Para activar todas las rutas, descomentar la siguiente línea:
-// export default flatRoutes() satisfies RouteConfig;
-
-// Configuración temporal: Todas las rutas muestran HomeScreen
 export default [
   route("*", "routes/_index.tsx"),
 ] satisfies RouteConfig;
