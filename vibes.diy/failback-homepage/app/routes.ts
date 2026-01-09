70e2858a4058da0633017d5e00deaa2d3c7e0bc6
Amber Macias <arussek05@gmail.com>
2026-01-09 16:54:29 -0600

    always redirect to homescreen

--- vibes.diy/failback-homepage/app/routes.ts
+++ vibes.diy/failback-homepage/app/routes.ts
@@ -1,5 +1,3 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-
-export default [
-  index("routes/_index.tsx"),
-] satisfies RouteConfig;
+import { type RouteConfig, route } from "@react-router/dev/routes";
+// Original routes: import { flatRoutes } from "@react-router/fs-routes"; export default flatRoutes() satisfies RouteConfig;
+export default [route("*", "routes/catch-all.tsx")] satisfies RouteConfig;
