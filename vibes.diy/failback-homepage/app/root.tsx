58786ff6f41a51331ad6b110396f697b55de63de
J Chris Anderson <jchris@fireproof.storage>
2026-01-08 21:19:31 -0800

    fix(failback-homepage): add ClerkProvider to root to support useAuth

--- vibes.diy/failback-homepage/app/root.tsx
+++ vibes.diy/failback-homepage/app/root.tsx
@@ -2,35 +2,43 @@ import React, { StrictMode } from "react";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import { PostHogProvider } from "posthog-js/react";
+import { ClerkProvider } from "@clerk/clerk-react";
 import { VibesDiyEnv } from "./config/env.js";
 import ClientOnly from "./components/ClientOnly.js";
 import CookieBanner from "./components/CookieBanner.js";
 import { CookieConsentProvider } from "./contexts/CookieConsentContext.js";
 import { ThemeProvider } from "./contexts/ThemeContext.js";
 import { ErrorBoundary } from "./ErrorBoundary.js";
+import { getClerkKeyForHostname } from "../clerk-env.js";
 
 // Only import Home for the failback homepage - other routes disabled
 import Home from "./routes/home.js";
 
 function RawApp({ children }: { children?: React.ReactNode }) {
+  const clerkPubKey = typeof window !== "undefined" 
+    ? getClerkKeyForHostname(window.location.hostname) 
+    : getClerkKeyForHostname("localhost");
+
   return (
-    <ThemeProvider>
-      <PostHogProvider
-        apiKey={VibesDiyEnv.POSTHOG_KEY()}
-        options={{
-          api_host: VibesDiyEnv.POSTHOG_HOST(),
-          opt_out_capturing_by_default: true,
-        }}
-      >
-        <CookieConsentProvider>
-          {children}
-          <Outlet />
-          <ClientOnly>
-            <CookieBanner />
-          </ClientOnly>
-        </CookieConsentProvider>
-      </PostHogProvider>
-    </ThemeProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <ThemeProvider>
+        <PostHogProvider
+          apiKey={VibesDiyEnv.POSTHOG_KEY()}
+          options={{
+            api_host: VibesDiyEnv.POSTHOG_HOST(),
+            opt_out_capturing_by_default: true,
+          }}
+        >
+          <CookieConsentProvider>
+            {children}
+            <Outlet />
+            <ClientOnly>
+              <CookieBanner />
+            </ClientOnly>
+          </CookieConsentProvider>
+        </PostHogProvider>
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
 
