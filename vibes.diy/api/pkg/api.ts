4019377cda34bd9e129acb0bbe38dec439ba202f
Meno Abels <meno.abels@adviser.com>
2026-01-26 06:10:41 +0100

    Revert "feat: add user slug claim/list endpoints with profile metadata"
    This reverts commit a9ed19d1de3aa9b1113e363d684436abd3f1a26c.

--- vibes.diy/api/pkg/api.ts
+++ vibes.diy/api/pkg/api.ts
@@ -2,13 +2,9 @@ import {
   ReqEnsureAppSlug,
   ReqEnsureChatContext,
   ReqAppendChatSection,
-  ReqClaimUserSlug,
-  ReqListUserSlugs,
   ResEnsureAppSlug,
   ResEnsureChatContext,
   ResAppendChatSection,
-  ResClaimUserSlug,
-  ResListUserSlugs,
   VibesDiyError,
 } from "@vibes.diy/api-types";
 import { Result } from "@adviser/cement";
@@ -17,6 +13,4 @@ export interface VibesDiyApiIface<Ops = object> {
   ensureAppSlug(req: ReqEnsureAppSlug & Ops): Promise<Result<ResEnsureAppSlug, VibesDiyError>>;
   ensureChatContext(req: ReqEnsureChatContext & Ops): Promise<Result<ResEnsureChatContext, VibesDiyError>>;
   appendChatSection(req: ReqAppendChatSection & Ops): Promise<Result<ResAppendChatSection, VibesDiyError>>;
-  claimUserSlug(req: ReqClaimUserSlug & Ops): Promise<Result<ResClaimUserSlug, VibesDiyError>>;
-  listUserSlugs(req: ReqListUserSlugs & Ops): Promise<Result<ResListUserSlugs, VibesDiyError>>;
 }
