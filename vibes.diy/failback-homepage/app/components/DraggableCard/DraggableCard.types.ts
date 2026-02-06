7c8f6614dcac9748c7a69b3144dc7513bd6acbf6
J Chris Anderson <jchris@fireproof.storage>
2026-02-06 11:10:51 -0800

    chore: delete old failback-homepage impl

--- vibes.diy/failback-homepage/app/components/DraggableCard/DraggableCard.types.ts
+++
@@ -1,9 +0,0 @@
-export type CardColor = "yellow" | "red" | "blue" | "grey";
-
-export interface DraggableCardProps {
-  children: React.ReactNode;
-  color: CardColor;
-  isText?: boolean;
-  x?: number;
-  y?: number;
-}
