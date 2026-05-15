db5e3af4e9436bd4cb4e7b7f839db1eb1fd4f72e
J Chris Anderson <jchris@fireproof.storage>
2026-05-15 05:58:15 -0700

    growth report

--- vibes.diy/api/svc/vibes-req-res-evento.ts
+++ vibes.diy/api/svc/vibes-req-res-evento.ts
@@ -17,6 +17,7 @@ import { filesAsset } from "./public/files-asset.js";
 import { putAsset } from "./public/put-asset.js";
 import { authSession, authLogout, authBridgePreflight } from "./public/asset-session.js";
 import { userAvatar } from "./public/get-user-avatar.js";
+import { reportGrowthMemberships } from "./public/report-growth-memberships.js";
 
 export const vibesReqResEvento = Lazy(() => {
   const evento = new Evento(new ReqResEventoEnDecoder());
@@ -49,6 +50,7 @@ export const vibesReqResEvento = Lazy(() => {
     putAsset,
     authSession,
     authLogout,
+    reportGrowthMemberships,
     servEntryPoint,
     {
       type: EventoType.WildCard,
